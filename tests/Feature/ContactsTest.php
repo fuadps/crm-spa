f495de4fff0cbc3a37b9862dbeccc5bba2ebf189
Fuad <fuadpls@gmail.com>
2020-03-11 01:35:52 +0800

    basic contact validation

--- tests/Feature/ContactsTest.php
+++ tests/Feature/ContactsTest.php
@@ -15,9 +15,43 @@ class ContactsTest extends TestCase
     public function a_contact_can_be_added()
     {
         $this->withoutExceptionHandling();
-        $this->post('/api/contacts', ['name' => 'John Doe']);
+        $this->post('/api/contacts', [
+            'name' => 'John Doe',
+            'email' => 'johndoe@email.com',
+            'birthday' => '23/07/1997',
+            'company' => 'Foo Company'
+        ]);
 
-        $this->assertCount(1, Contact::all());
+        $contact = Contact::first();
+        $this->assertEquals('John Doe', $contact->name);
+        $this->assertEquals('johndoe@email.com', $contact->email);
+        $this->assertEquals('23/07/1997', $contact->birthday);
+        $this->assertEquals('Foo Company', $contact->company);
+    }
 
+    /** @test */
+    public function a_name_is_required()
+    {
+        $response = $this->post('/api/contacts', [
+            'email' => 'johndoe@email.com',
+            'birthday' => '23/07/1997',
+            'company' => 'Foo Company'
+        ]);
+
+        $response->assertSessionHasErrors('name');
+        $this->assertCount(0, Contact::all());
+    }
+
+    /** @test */
+    public function email_is_required()
+    {
+        $response = $this->post('/api/contacts', [
+            'name' => 'John Doe',
+            'birthday' => '23/07/1997',
+            'company' => 'Foo Company'
+        ]);
+
+        $response->assertSessionHasErrors('email');
+        $this->assertCount(0, Contact::all());
     }
 }
