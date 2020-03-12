cb261bdefe834234b92bceefbca859d6a5931d67
Fuad <fuadpls@gmail.com>
2020-03-13 00:11:49 +0800

    TTD - Email and birthday in right format

--- tests/Feature/ContactsTest.php
+++ tests/Feature/ContactsTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Contact;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -10,7 +11,7 @@ use Tests\TestCase;
 class ContactsTest extends TestCase
 {
     use RefreshDatabase;
-    
+
     /** @test */
     public function a_contact_can_be_added()
     {
@@ -28,15 +29,35 @@ class ContactsTest extends TestCase
     public function fields_are_required()
     {
         collect(['name', 'email', 'birthday', 'company'])
-            ->each(function($field) {
+            ->each(function ($field) {
                 $response = $this->post('/api/contacts', array_merge($this->data(), [$field => '']));
-        
+
                 $response->assertSessionHasErrors($field);
                 $this->assertCount(0, Contact::all());
             });
     }
 
-    protected function data() 
+    /** @test */
+    public function email_must_be_valid_format()
+    {
+        $response = $this->post('/api/contacts', array_merge($this->data(), ['email' => 'Not an email']));
+
+        $response->assertSessionHasErrors('email');
+        $this->assertCount(0, Contact::all());
+    }
+
+    /** @test */
+    public function birthday_are_properly_stored()
+    {
+        $this->withoutExceptionHandling();
+        $response = $this->post('/api/contacts', array_merge($this->data(),['birthday' => '17 May 2020']));
+
+        $this->assertCount(1, Contact::all());
+        $this->assertInstanceOf(Carbon::class, Contact::first()->birthday);
+        $this->assertEquals('17-05-2020', Contact::first()->birthday->format('d-m-Y'));
+    }
+
+    protected function data()
     {
         return [
             'name' => 'John Doe',
